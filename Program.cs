dc3b40ab5d1aca61c76713ba8e316ace46c96c77
Rodrigo Roma <rodrigo.rmartins@gmail.com>
2023-07-10 20:06:40 -0300

    Solução

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Zapzap");
+
+Console.WriteLine("\n\n");
+
+Smartphone iphone = new Nokia("654321", "Modelo 2", "222222222", 16);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("VaptVupt");
